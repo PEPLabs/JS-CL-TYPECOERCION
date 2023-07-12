db0a3cdc6ff0ef8553eecd0ee3909f53dbfa753b
LiquidPlummer <kyle.plummer@revature.com>
2023-07-12 11:50:17 -0400

    final fixes

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -20,7 +20,7 @@ public class SeleniumTest {
     @BeforeEach
     void setup() {
         // Set up ChromeDriver path
-        System.setProperty("webdriver.chrome.driver", "/driver/chromedriver");//linux_64
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");//linux_64
 
         // Get file
         File file = new File("TypeCoercion.html");
